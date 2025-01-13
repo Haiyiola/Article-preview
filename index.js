89747cd491873c0cdb9c7d747687239bf998a724
Haiyiola <154631429+Haiyiola@users.noreply.github.com>
2025-01-13 14:57:55 +0100

    Added the HTML, CSS files

---
+++ index.js
@@ -0,0 +1,5 @@
+let share = document.getElementById("share")
+let socialsSection = document.getElementById("socials")
+share.addEventListener("click", function(){
+    socialsSection.style.display= "block"
+})
